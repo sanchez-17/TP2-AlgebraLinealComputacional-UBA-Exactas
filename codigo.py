e78c1d57208f07b0ec2ca0392290d4c31ccbdc33
juanifiore <juanifiore291@gmail.com>
2023-06-04 20:33:47 -0300

    c

---
+++ codigo.py
@@ -0,0 +1,57 @@
+'''
+TP2 ALC
+'''
+
+import numpy as np
+import pandas as pd
+
+#==============================================================================
+# EJERCICIO 1
+#==============================================================================
+
+#-----------------------------------------------------------------------------
+#(a) Realizar una funcion en python que dado los datos de las imagenes de entrenamiento 
+#y una fila, grafique la imagen guardada en esa fila y en el tıtulo del grafico se 
+#indique a que numero corresponde, es decir su clasificacion. Usar la funcion imshow() de pyplot.
+#-----------------------------------------------------------------------------
+
+
+
+#-----------------------------------------------------------------------------
+#(b) ¿Cuantas im ́agenes hay por cada d ́ıgito en el conjunto de entrenamiento? ¿Y en el conjunto
+#de testeo?
+#-----------------------------------------------------------------------------
+
+
+
+#-----------------------------------------------------------------------------
+#(c) Para las primeras 2.000 imagenes del conjunto de entrenamiento realizar una funcion en python
+#que devuelva la imagen promedio de cada uno de los dıgitos.
+#-----------------------------------------------------------------------------
+
+
+
+#-----------------------------------------------------------------------------
+#(d) Graficar cada una de las imagenes promedio obtenidas.
+#-----------------------------------------------------------------------------
+
+
+
+
+#==============================================================================
+# EJERCICIO 2
+#==============================================================================
+
+#-----------------------------------------------------------------------------
+
+
+(a) Realizar una funci ́on en python que dadas las im ́agenes promedio del ejercicio 2(c), calcule la
+menor distancia Eucl ́ıdea entre todos los d ́ıgitos y cada una de las primeras 200 im ́agenes de
+testeo. La funci ́on debe devolver un arreglo con las 200 predicciones.
+(b) Realizar una funci ́on en python que tome el arreglo de predicciones anteriores y eval ́ue si es
+correcta o no la predicci ́on. Debe devolver la precisi ́on en la predicci ́on. Se define la precisi ́on
+como:
+P recisi ́on = Σ(Casos acierto)
+Σ(Casos totales)
+(c) Graficar un par de casos de im ́agenes de testeo en los cuales no se haya acertado. ¿Considera
+buena la precisi ́on?
